70f7709e847e4ddcbc25c26569b49f8e46ad22fb
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2022-06-02 19:31:18 -0500

    while loop

--- asm/bf.s
+++ asm/bf.s
@@ -9,8 +9,8 @@ print:
   push rcx
   mov rax, 1        	; write
   mov rdi, 1        	; STDOUT_FILENO
-  lea rsi, [rcx+tape]
-  mov rdx, 1
+  lea rsi, [rcx+tape]	; from current cell
+  mov rdx, 1			; write 1 byte
   syscall
   pop rcx
   ret
@@ -19,7 +19,7 @@ read:
   push rcx
   mov rax, 0        	; read
   mov rdi, 0        	; STDIN_FILENO
-  lea rsi, [rcx+tape]
+  lea rsi, [rcx+tape]	; to current cell
   mov rdx, 1        	; read 1 byte
   syscall
   pop rcx
@@ -27,8 +27,8 @@ read:
 
 _exit:
   mov rax, 60       	; exit
-  mov rdi, 0
-  syscall
+  mov rdi, 0			; 0 is success
+  syscall				; finish
 
 _start:
   xor rcx, rcx			; initialize tape pointer in rcx
@@ -89,5 +89,28 @@ _start:
   dec rcx				; <
 
   call print			; .
+
+  inc byte [rcx+tape]	; +
+
+  ; setup while loop
+  inc rcx				; >
+  inc rcx				; >
+  inc byte [rcx+tape]	; +
+  inc byte [rcx+tape]	; +
+  inc byte [rcx+tape]	; +
+
+  ; while loop 3 times
+l1:						; ]
+  jz exit_l1			; ]
+
+  dec rcx				; <
+  dec rcx				; <
+  call print			; .
+  inc rcx				; >
+  inc rcx				; >
+  dec byte [rcx+tape]	; -
+
+  jmp l1				; [
+exit_l1:				; [
 
   call _exit
